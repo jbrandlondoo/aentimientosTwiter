8e95228aa8cc958262552d3eacbb77fd8507152f
JOSEBRANDL <jbrandlondoo@yahoo.es>
2019-05-26 22:10:12 -0500

    flask implement

--- Sin título0.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May 26 18:00:45 2019
-
-@author: ASUS-PC
-"""
-
--- main.py
+++ main.py
@@ -1,59 +1,67 @@
-def ClearData(text):
-	text = text.replace(",","")
-	text = text.replace(".","")
-	text = text.replace("/","")
-	text = text.replace(">","")
-	text = text.replace("<","")
-	text = text.replace('"',"")
-	text = text.replace("'","")
-	text = text.replace("1","")
-	text = text.replace("2","")
-	text = text.replace("3","")
-	text = text.replace("4","")
-	text = text.replace("5","")
-	text = text.replace("6","")
-	text = text.replace("7","")
-	text = text.replace("8","")
-	text = text.replace("9","")
-	text = text.replace("0","")
-	text = text.replace("*","")
-	text = text.replace("!","")
-	text = text.replace("(","")
-	text = text.replace(")","")
-	text = text.replace(";","")
-	text = text.replace(":","")
-	return text
-
-
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 import pandas as pd 
 import numpy as np 
 
-data = []
-multi = MultinomialNB()
+class modelo():
+	data = []
+	multi = MultinomialNB()
 
-with open('dataset.csv','r') as file:
-	lineas = file.read().splitlines()
-	i = 0
-	for l in lineas:
-		if i == 0:
-			i += 1
-		else:
-			l = l.split(',')
-			data.append([ClearData("".join(l)),l[len(l)-1]]);
+	def __init__(self, arg):
+		super(modelo, self).__init__()
+		self.arg = arg
+		entrenar()
+		
 
-data = pd.DataFrame(data)
+	def ClearData(text):
+		text = text.replace(",","")
+		text = text.replace(".","")
+		text = text.replace("/","")
+		text = text.replace(">","")
+		text = text.replace("<","")
+		text = text.replace('"',"")
+		text = text.replace("'","")
+		text = text.replace("1","")
+		text = text.replace("2","")
+		text = text.replace("3","")
+		text = text.replace("4","")
+		text = text.replace("5","")
+		text = text.replace("6","")
+		text = text.replace("7","")
+		text = text.replace("8","")
+		text = text.replace("9","")
+		text = text.replace("0","")
+		text = text.replace("*","")
+		text = text.replace("!","")
+		text = text.replace("(","")
+		text = text.replace(")","")
+		text = text.replace(";","")
+		text = text.replace(":","")
+		return text
 
-from sklearn.model_selection import train_test_split
-xTrain, xTest, yTrain, yTest = train_test_split(data[0], data[1], test_size = 0.008)
+	def setData(datset):
+		dataH = []
+		with open(datset,'r') as file:
+			lineas = file.read().splitlines()
+			i = 0
+			for l in lineas:
+				if i == 0:
+					i += 1
+				else:
+					l = l.split(',')
+					dataH.append([ClearData("".join(l)),l[len(l)-1]]);
+		dataH = pd.DataFrame(dataH)
+		data = dataH
 
-from sklearn.feature_extraction.text import HashingVectorizer
-hashingV = HashingVectorizer(stop_words='english',alternate_sign=False)
-xTrain = hashingV.transform(xTrain)
-xTest = hashingV.transform(xTest)
-
-
-from sklearn.metrics import accuracy_score
-multi.fit(xTrain,yTrain)
-score = accuracy_score(yTest,multi.predict(xTest))
-print(score)
+	def entrenar():
+		setData('dataset.csv')
+		xTrain, xTest, yTrain, yTest = train_test_split(data[0], data[1], test_size = 0.008)
+		hashingV = HashingVectorizer(stop_words='english',alternate_sign=False)
+		xTrain = hashingV.transform(xTrain)
+		xTest = hashingV.transform(xTest)
+		multi.fit(xTrain,yTrain)
+		score = accuracy_score(yTest,multi.predict(xTest))
+		print(score)
+	
