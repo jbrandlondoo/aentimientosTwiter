10808baaf276326a1d0826e5f3a2afd7f7c5ac01
JOSEBRANDL <jbrandlondoo@yahoo.es>
2019-05-27 16:51:31 -0500

    v2

--- readTrain.py
+++ readTrain.py
@@ -1,21 +1,14 @@
+from pathlib import Path
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pandas as pd 
 import numpy as np 
+import os
+import xgboost as xgb
 
-class modelo():
-	data = []
-	multi = MultinomialNB()
-
-	def __init__(self, arg):
-		super(modelo, self).__init__()
-		self.arg = arg
-		entrenar()
-		
-
-	def ClearData(text):
+def ClearData(text):
 		text = text.replace(",","")
 		text = text.replace(".","")
 		text = text.replace("/","")
@@ -42,15 +35,39 @@ class modelo():
 		return text
 
 dataH = []
-with open(datset,'r') as file:
-	lineas = file.read().splitlines()
-	i = 0
-	for l in lineas:
-		if i == 0:
-			i += 1
-		else:
-			l = l.split(',')
-			dataH.append([ClearData("".join(l)),l[len(l)-1]]);
+
+from os import scandir
+path = [r".\neg",r".\pos"]
+i = 0
+for p in path:
+	lista_de_archivos = os.listdir(p)
+	for d in lista_de_archivos:
+		try:
+			with open(p+r"\\"+d,'r') as file:
+				lineas = file.read()
+				dataH.append([ClearData(lineas),i]);
+		except Exception as e:
+			pass
+
+	i += 1
+
 dataH = pd.DataFrame(dataH)
+multi = MultinomialNB()
+xTrain, xTest, yTrain, yTest = train_test_split(dataH[0], dataH[1], test_size = 0.008)
 hashingV = HashingVectorizer(stop_words='english',alternate_sign=False)
-model.predit()
+xTrain = hashingV.transform(xTrain)
+xTest = hashingV.transform(xTest)
+multi.fit(xTrain,yTrain)
+score = accuracy_score(yTest,multi.predict(xTest))
+print(score)
+
+modelo= xgb.XGBClassifier()
+modelo.fit(xTrain,yTrain)
+
+#prediccion
+y_pred=modelo.predict(xTrain)
+prediccciones= [round(value) for value in y_pred]
+precision_train=accuracy_score(yTrain,prediccciones)
+
+print(precision_train)
+
